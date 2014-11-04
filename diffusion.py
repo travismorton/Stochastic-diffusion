4212ee4f1bebd86e94c14ad2bb87a43d0257d4ef
travismorton <travismorton1@gmail.com>
2014-11-04 16:39:16 -0500

    Reduced lines

--- diffusion.py
+++ diffusion.py
@@ -1,10 +1,9 @@
-import numpy
+import numpy, math, random
+walkers, time, timeStep = 1000, 100, .1
+steps = int(round(time / timeStep))
+particles, eta = numpy.random.normal(-3, 1, walkers), numpy.random.normal(0, math.sqrt(timeStep), 1000000)
+for i in range(steps):
+    for j in range(walkers):
+        particles[j] = particles[j] - particles[j] * timeStep + random.choice(eta)
 
-walkers = 10000
-time = 100
-timeStep = .1
 
-# initialize gaussian initial distribution
-
-# walk walkers for total time
-for i in range(time):
